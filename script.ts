fd7e6dfd4e7af8b6439ce06bca7f09a215d616ae
YOSIMOSH <yzm62269@gmail.com>
2024-09-19 10:28:03 +0300

    Up to this point, I built the search form and the search results table in HTML, and design in CSS

---
+++ script.ts
@@ -0,0 +1,17 @@
+//Player data interface
+interface Player {
+  position: string;
+  twoPercent: Number;
+  threePercent: Number;
+  points: Number;
+  playerName: String;
+}
+
+//API link for basketball players
+const BASE_URL = 'https://nbaserver-q21u.onrender.com/';
+
+
+
+
+
+
